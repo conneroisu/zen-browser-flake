b2959d74a07d793dec98b483d459af75e2098215
connerohnesorge <conneroisu@outlook.com>
2025-01-29 11:03:34 -0600

    latest

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,17 @@
-{ pkgs, lib, config, inputs, ... }:
-
 {
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
   # https://devenv.sh/basics/
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+  ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
